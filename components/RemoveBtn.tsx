0e4c48278b157b0db581c7de3b0fdbac6264f2c3
tlc2880 <tommylcao@gmail.com>
2023-12-25 04:38:57 -0800

    Add addTopic RemoveBtn

--- components/RemoveBtn.tsx
+++ components/RemoveBtn.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { HiOutlineTrash } from "react-icons/hi"
+"use client"
+import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation"
 
-export default function RemoveBtn() {
+type Props = {
+  id: string
+}
+
+export default function RemoveBtn({ id }: Props) {
+  const router = useRouter();
+  const removeTopic = async () => {
+    const confirmed = confirm("Are you sure?");
+
+    if (confirmed) {
+      const res = await fetch(`https://main.d22i920ywfkorh.amplifyapp.com/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
+
+      if (res.ok) {
+        router.refresh();
+      }
+    }
+  };
+
   return (
-    <button className="text-red-400">
+    <button onClick={removeTopic} className="text-red-400">
       <HiOutlineTrash size={24} />
     </button>
-  )
+  );
 }
