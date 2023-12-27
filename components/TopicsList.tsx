1e3f89e6c95d51e5e3a9003d86e9cac8f4cd7d2f
tlc2880 <tommylcao@gmail.com>
2023-12-27 06:42:46 -0800

    Add dark-mode

--- components/TopicsList.tsx
+++ components/TopicsList.tsx
@@ -26,15 +26,15 @@ export default async function TopicsList() {
     {topics.map((t: topicType) => (
       <div key={t._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
         <div>
-          <h2 className="font-bold text-2xl">{t.title}</h2>
+          <h2 className="font-bold text-xl">{t.title}</h2>
           <div>{t.description}</div>
         </div>
-        <div className="flex gap-2">
-          <RemoveBtn id={t._id}/>
-          <Link href={`/editTopic/${t._id}`}>
-            <HiPencilAlt size={24} />
-          </Link>
-        </div>
+        <div className="flex gap-1">
+            <RemoveBtn id={t._id}/>
+            <Link className="py-3 l-fit" href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
       </div>
       ))}
     </>
